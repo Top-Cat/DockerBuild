40467e73acd921a2b42445cedcaf2db20bb1f611
Thomas Cheyney <top_cat@thomasc.co.uk>
2019-11-29 11:05:29 +0000

    Fork

--- nginx/reload.sh
+++ nginx/reload.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-nginx "$@"
+nginx "$@" &
 oldcksum=`find /etc/nginx/conf.d -type f -exec md5sum {} \; | sort -k 2 | md5sum`
 
 while true; do
@@ -8,7 +8,7 @@ while true; do
 
 	newcksum=`find /etc/nginx/conf.d -type f -exec md5sum {} \; | sort -k 2 | md5sum`
 	if [ "$newcksum" != "$oldcksum" ]; then
-		echo "At ${time} on ${date}, config file update detected."
+		echo "On $(date), config file update detected."
 		oldcksum=$newcksum
 		nginx "$@" -s reload
 	fi
