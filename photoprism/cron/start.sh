5816b03fa084db0833e764150820787e22d17010
Thomas Cheyney <top_cat@thomasc.co.uk>
2024-01-13 21:43:21 +0000

    Add photoprism with cron

---
+++ photoprism/cron/start.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+LOG="/var/log/photoprism-cron.log"
+CRONTAB="/photoprism/cron/crontab"
+
+if [ "$PHOTOPRISM_AUTO_INDEX_CRON" = "" ]; then
+  PHOTOPRISM_AUTO_INDEX_CRON="* 6 * * *"
+fi
+
+touch "$LOG"
+
+printf "$PHOTOPRISM_AUTO_INDEX_CRON /photoprism/cron/index.sh 2>> $LOG\n" > "$CRONTAB"
+crontab "$CRONTAB"
+
+/photoprism/cron/generate-cron.sh
+
+rsyslogd
+cron
+photoprism start
