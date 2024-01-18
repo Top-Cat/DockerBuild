83d5fca387ee24772ad84cd51b6140b509640c15
Thomas Cheyney <top_cat@thomasc.co.uk>
2024-01-18 08:14:40 +0000

    Don't pass cron config to photoprism

--- photoprism/cron/generate-cron.sh
+++ photoprism/cron/generate-cron.sh
@@ -4,7 +4,7 @@ WORKDIR=/photoprism/cron
 SCRIPT=$WORKDIR/index.sh
 
 shebang="#!/usr/bin/env bash";
-env=$(printenv | sed 's/^\(.*\)$/export \1/g' | grep -E '^export PHOTOPRISM' | awk '{ printf("%s;\\n", $0) }')
+env=$(printenv | grep -v 'PHOTOPRISM_AUTO_INDEX_CRON' | sed 's/^\(.*\)$/export \1/g' | grep -E '^export PHOTOPRISM' | awk '{ printf("%s;\\n", $0) }')
 run="/photoprism/bin/photoprism index";
 
 generated=$shebang$"\n\n"$env$"\n\n"$run;
