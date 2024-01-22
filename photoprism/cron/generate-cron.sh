f97355f29ae2d86dc1d2dcf0d486365c97ea8cbf
Thomas Cheyney <top_cat@thomasc.co.uk>
2024-01-22 12:55:57 +0000

    Don't hardcode photoprism binary

--- photoprism/cron/generate-cron.sh
+++ photoprism/cron/generate-cron.sh
@@ -5,7 +5,8 @@ SCRIPT=$WORKDIR/index.sh
 
 shebang="#!/usr/bin/env bash";
 env=$(printenv | grep -v 'PHOTOPRISM_AUTO_INDEX_CRON' | sed 's/^\(.*\)$/export \1/g' | grep -E '^export PHOTOPRISM' | awk '{ printf("%s;\\n", $0) }')
-run="/photoprism/bin/photoprism index";
+bin=$(which photoprism)
+run=$bin" index";
 
 generated=$shebang$"\n\n"$env$"\n\n"$run;
 echo -e $generated > $SCRIPT
